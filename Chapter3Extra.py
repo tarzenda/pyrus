59cc1c7dace9af20b80cb0ae6fbe273ab22ba82d
tarzenda <code@tarzenda.net>
2014-09-07 23:08:58 -0500

    Added chapter 3 extra sample.

---
+++ Chapter3Extra.py
@@ -0,0 +1,28 @@
+print("starting program")
+
+x=1
+y=2
+z=3
+
+def foo():
+    print("entering foo()")
+    x=10
+    y=20
+    bar()
+    # print(x)
+    print("exiting foo()")
+
+def bar():
+    print("entering bar()")
+    x = 30
+    # print(x)
+    baz()
+    print("exiting bar()")
+
+def baz():
+    print("entering baz()")
+    # print(x)
+    print("exiting baz()")
+
+foo()
+print("ending program")

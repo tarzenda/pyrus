adebdc4ab4937faea1ef6d3d8ab6fd30d66aeff5
psugg <chezwhiz@gmail.com>
2014-09-10 22:48:57 -0500

    These are all the things that I have done so far

---
+++ excercise 4.py
@@ -0,0 +1,2 @@
+# this is a comment that I am writing that will not show when the script runs
+print ("this is an example to show that comments do not show when running a script!")
---
+++ import_test.py
@@ -0,0 +1,4 @@
+
+def cat_n_times(s, n):
+    cat = s * n
+    print(cat)

51dfa427df67f137a8d751a3bbdbee9801cbecff
tarzenda <code@tarzenda.net>
2014-10-19 01:57:10 -0500

    Modified assn 4, ex 18 and 19 to include prompt within tax_calc() function body.

--- assn4/ex19.py
+++ assn4/ex19.py
@@ -1,12 +1,11 @@
-print ("Please enter sales total.")
-sales_total = input()
 sales_tax_percent = 9
 
 def tax_calc():
+    print ("Please enter sales total.")
+    sales_total = input()
     intSales_total = int(sales_total) * 100
     sales_tax = intSales_total * sales_tax_percent
     grand_total = float(sales_tax) / 100 + float(intSales_total)
     print ("Sales total: ", float(sales_total))
     print ("Sales tax: ", float(sales_tax) / 10000)
     print ("Grand total: ", float(grand_total) /100)
-tax_calc()

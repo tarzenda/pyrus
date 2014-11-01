b5bcefa6ecdb782cb9c47cc246f1bfb709a78740
tarzenda <code@tarzenda.net>
2014-11-01 12:06:03 -0500

    assn4 ex19 corrected

--- assn4/ex19.py
+++ assn4/ex19.py
@@ -2,10 +2,15 @@ sales_tax_percent = 9
 
 def tax_calc():
     print ("Please enter sales total.")
-    sales_total = input()
-    intSales_total = int(sales_total) * 100
-    sales_tax = intSales_total * sales_tax_percent
-    grand_total = float(sales_tax) / 100 + float(intSales_total)
-    print ("Sales total: ", float(sales_total))
-    print ("Sales tax: ", float(sales_tax) / 10000)
-    print ("Grand total: ", float(grand_total) /100)
+    sales_total_float = float(input())
+
+    # calculate results in pennies...
+    sales_total = int(sales_total_float * 100.0)
+    sales_tax = sales_total * sales_tax_percent
+    grand_total = sales_total + sales_tax
+
+    # print results in floating...
+    print ("Sales total: ", sales_total/100.0)
+    print ("Sales tax: ", sales_tax/100.0)
+    print ("Grand total: ", grand_total/100.0)
+

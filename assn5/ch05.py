7fa232b0297fe76c9e1b49d3801b586a49f17bb7
tarzenda <code@tarzenda.net>
2014-11-01 17:41:03 -0500

    Adding beginning of exercise 1

--- assn5/ch05.py
+++ assn5/ch05.py
@@ -1,2 +1,20 @@
 # http://openbookproject.net/thinkcs/python/english2e/ch05.html
 # Do chapter 5 exercises in this file.
+
+# Shared imports
+import math
+
+# Exercise 1
+def compare(a, b):
+    """
+      >>> compare(5, 4)
+      1
+      >>> compare(7, 7)
+      0
+      >>> compare(2, 3)
+      -1
+      >>> compare(42, 1)
+      1
+    """
+    #  Your function body should begin here.
+    pass
